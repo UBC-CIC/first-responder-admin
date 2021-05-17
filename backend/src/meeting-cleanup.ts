adbcfecdc08b8bd32d3893b761f6076249a414f5
Tom Jin <tbj128@users.noreply.github.com>
2021-05-17 00:30:22 -0700

    Fix bug with updating specialist status

--- backend/src/meeting-cleanup.ts
+++ backend/src/meeting-cleanup.ts
@@ -18,7 +18,7 @@ export const handler = async (event: any = {}, context: any, callback: any): Pro
         const dao = new MeetingDetailsDao(db);
         await dao.endMeeting(meetingId);
         console.log(`Meeting ended...`);
-        updateSpecialistStatusIfApplicable(dao, meetingId);
+        await updateSpecialistStatusIfApplicable(dao, meetingId);
     } else if (eventType === "chime:AttendeeLeft") {
         console.log(`Found eventType attendee left...`);
         const attendeeId = event["detail"]["attendeeId"];
@@ -26,15 +26,18 @@ export const handler = async (event: any = {}, context: any, callback: any): Pro
         const dao = new MeetingDetailsDao(db);
         await dao.attendeeLeft(meetingId, attendeeId);
         console.log(`Attendee left...`);
-        updateSpecialistStatusIfApplicable(dao, meetingId, attendeeId);
+        await updateSpecialistStatusIfApplicable(dao, meetingId, attendeeId);
     }
 };
 
 const updateSpecialistStatusIfApplicable = async (dao: MeetingDetailsDao, meetingId: string, attendeeId: string | null = null): Promise<any> => {
     console.log(`Cleaning up the status of any paged specialists for meeting ${meetingId} and attendeeId ${attendeeId}`);
     const meeting = await dao.getMeetingWithMeetingId(meetingId);
+    console.log(`Found meeting ${meeting}`);
     for (let attendee of meeting.attendees) {
+        console.log(`Working on attendee ${attendee}`);
         if (attendee.attendee_type === AttendeeType.SPECIALIST) {
+            console.log(`Found specialist`);
             if (!attendeeId || attendeeId === attendee.attendee_id) {
                 console.log(`Found specialist to cleanup: ${attendee.phone_number}`);
                 const specialistProfileDao = new SpecialistProfileDao(db);
